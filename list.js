a6561b0eebca3903081c1f79e3219ed12ffc508a
Ingoshe <163987765+Ingoshe@users.noreply.github.com>
2024-04-08 12:57:15 +0300

    expandable list

---
+++ list.js
@@ -0,0 +1,25 @@
+// const cars = document.querySelector('.cars');
+// const tyres = document.querySelector('.tyres');
+// const events = document.querySelector('.events');
+// const first = document.querySelector('.first');
+// const second = document.querySelector('.second');
+// const third = document.querySelector('.third');
+
+first.addEventListener('click', function(){
+    
+    cars.classList.toggle("car");
+   
+})
+
+second.addEventListener('click', function(){
+ 
+    tyres.classList.toggle("car");
+    
+})
+
+third.addEventListener('click', function(){
+   
+    events.classList.toggle("event");
+    
+
+})
